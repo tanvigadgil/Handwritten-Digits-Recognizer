53641e444a0db86e95fd6a1b816ab62f47714239
Tanvi Gadgil <65287714+tanvigadgil@users.noreply.github.com>
2020-12-11 02:13:33 +0530

    Added Loading Page with navigation

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+        return render_template('index.html')
 
-@app.route('/draw', methods = ['POST', 'GET'])
-def drawAndPredict():
-    return 
+@app.route('/draw')
+def draw():
+    return render_template('draw.html')
 
-@app.route('/upload', methods = ['POST', 'GET'])
-def uploadAndPredict():
-    return
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
